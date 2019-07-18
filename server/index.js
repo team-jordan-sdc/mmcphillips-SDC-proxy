2d56975c5b665051cbe358d28e74c6047b7a1bdb
Henry Mattoon <hmattoon@mail.sfsu.edu>
2019-07-17 21:11:09 -0700

    add api paths that fetch from each individual service

--- server/index.js
+++ server/index.js
@@ -1,19 +1,49 @@
 const express = require('express');
 const path = require('path');
 const bodyparser = require('body-parser');
+const fetch = require('node-fetch');
 
 const app = express();
 app.use(express.static(path.join(__dirname, '/../client/dist')));
+app.use(express.json());
 
 
 app.get('/api/rand', (req, res) => {
-  console.log('getting rand');
-  fetch('localhost:3000/api/rand')
+  fetch('http://localhost:3002/api/rand')
   .then(fres => fres.json())
   .then(fres =>  {
     res.status(200).send(fres);
   });
 });
+
+app.get('/api/personnel', (req, res) => {
+  fetch(`http://localhost:3000/api/personnel?id=${req.query.id}`)
+  .then(fres => fres.json())
+  .then(fres =>  {
+    res.status(200).send(fres);
+  });
+});
+
+app.get('/api/movies', (req, res) => {
+  fetch(`http://localhost:3000/api/movies?feature=true`)
+  .then(fres => fres.json())
+  .then(fres =>  {
+    res.status(200).send(fres);
+  });
+});
+
+app.get('/api/reviews', (req, res) => {
+  console.log(req.query.filmname)
+  fetch(`http://localhost:3001/api/reviews?filmname=The Shawshank Redemption`,{
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' }
+  })
+  .then(fres => fres.json())
+  .then(fres =>  {
+    res.status(200).send(fres);
+  });
+})
+
 
 
 const port = 8000;
